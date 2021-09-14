398a3192a85f6eb9af89550cfd20650537730c72
bkm0903 <bkm0903v@naver.com>
2021-09-14 22:30:40 +0900

    UI Drag 기능
    UI Drag 디테일

--- Capston/Assets/Scripts/UI Scripts/DragUI.cs
+++ Capston/Assets/Scripts/UI Scripts/DragUI.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragUI : MonoBehaviour, IDragHandler
+public class DragUI : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
+
+    private RectTransform dragRectTransform;
+    private Canvas canvas;
+
+    private void Awake()
+    {
+        //Cursor.lockState = CursorLockMode.Confined; = 커서가 화면 밖으로 나가지 않게 해줌
+
+        if (dragRectTransform == null)
+        {
+            dragRectTransform = transform.parent.GetComponent<RectTransform>();
+        }
+
+        if(canvas == null)
+        {
+            Transform testCanvasTransform = transform.parent;
+            while (testCanvasTransform != null)
+            {
+                canvas = testCanvasTransform.GetComponent<Canvas>();
+                if(canvas != null)
+                {
+                    break;
+                }
+                testCanvasTransform = testCanvasTransform.parent;
+            }
+        }
+    }
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = eventData.position;
+        dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        dragRectTransform.SetAsLastSibling();
     }
 }
--- Capston/Assets/Scripts/UI Scripts/PlayerSkillPanel.cs
+++ Capston/Assets/Scripts/UI Scripts/PlayerSkillPanel.cs
@@ -6,13 +6,11 @@ public class PlayerSkillPanel : MonoBehaviour
 {
     [SerializeField]
     private GameObject SkillPanel;
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.K))
