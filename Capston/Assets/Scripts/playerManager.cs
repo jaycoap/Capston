bd37de61999537566833014bb3005bacd67b09c1
nogunga <78949961+nogunga@users.noreply.github.com>
2021-10-15 13:22:21 +0900

    2021-10-15 PM 13:22
    점프 horizontal키에서 jump키로 변경
    이동 고정값으로 바꾸고 벽점프 안되게 만듬

--- Capston/Assets/Scripts/playerManager.cs
+++ Capston/Assets/Scripts/playerManager.cs
@@ -18,57 +18,83 @@ public class playerManager : MonoBehaviour
         animator = GetComponent<Animator>();
         isDead = false;
         isGrounded = false;
-        movementSpeed = 0.5f;
+        movementSpeed = 0.1f;
         jumpSpeed = 9f;
     }
 
     void Update()
     {
+        //Horizontal 버튼을 누를때 각 방향으로 캐릭터 스프라이트를 뒤집는 함수.
         if (Input.GetButton("Horizontal"))
         {
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
         }
-        if (Mathf.Abs( rigidBody.velocity.x ) < 0.9)
+        //걷고 있는지 체크해서 애니메이션을 관리하는 변수를 바꾸는 함수.
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
         {
-            animator.SetBool("isWalk", false);
+            animator.SetBool("isWalk", true);
         }
         else
         {
-            animator.SetBool("isWalk", true);
+            animator.SetBool("isWalk", false);
         }
     }
 
     void FixedUpdate()
     {
-
+        
         if (!isDead)
         {
             float H_input = Input.GetAxisRaw("Horizontal");
-            
+            //Horizontal버튼을 누르면 이동시키는 함수
             if(Input.GetButton("Horizontal"))
             {
-                //transform.Translate(Vector3.right * H_input * movementSpeed);
-                rigidBody.AddForce(Vector2.right * H_input * movementSpeed, ForceMode2D.Impulse);
+                if(H_input == 1)
+                {
+                    if (Physics2D.OverlapBox(new Vector2(transform.position.x + 0.229166675f, transform.position.y), new Vector2(0.45833335f, 1.4f), 0, LayerMask.GetMask("Floor")))
+                    {
+
+                    }
+                    else
+                    {
+                        transform.Translate(Vector2.right * H_input * movementSpeed);
+                    }
+                }
+                if (H_input == -1)
+                {
+                    if (Physics2D.OverlapBox(new Vector2(transform.position.x - 0.229166675f, transform.position.y), new Vector2(0.45833335f, 1.4f), 0, LayerMask.GetMask("Floor")))
+                    {
+
+                    }
+                    else
+                    {
+                        transform.Translate(Vector2.right * H_input * movementSpeed);
+                    }
+                }
+
+
 
             }
             
-            
+            //땅에 붙어있을 경우에만 점프를 시켜주는 함수
             if (isGrounded)
             {
                 
-                float V_input = Input.GetAxisRaw("Vertical");
+                float J_input = Input.GetAxisRaw("Jump");
 
-                if(V_input == 1)
+                if (J_input == 1)
                 {
-                    rigidBody.AddForce(Vector2.up * V_input * jumpSpeed, ForceMode2D.Impulse);
-                    
+                    if(rigidBody.velocity.y < 9)
+                    {
+                        rigidBody.AddForce(Vector2.up * J_input * jumpSpeed, ForceMode2D.Impulse);
+                    }  
                 }
 
-                
+
             }
         }
-
-        if (Physics2D.OverlapBox(transform.position, new Vector2(0.8f, 1.666667f), 0, LayerMask.GetMask("Floor")))
+        //overlapbox(1,2,3,4) 1번 위치에서 2번 크기의 범위에 3번 회전의 오브젝트가 4번 레이어이면 true
+        if (Physics2D.OverlapBox(new Vector2(transform.position.x,transform.position.y - 0.8333f), new Vector2(0.7f, 0.1f), 0, LayerMask.GetMask("Floor")))
         {
             isGrounded = true;
 
