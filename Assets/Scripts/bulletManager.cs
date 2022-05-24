5f5dae0b89e997fb5b1fd30f7469ffc0f9140c44
nogunga <78949961+nogunga@users.noreply.github.com>
2022-05-25 01:07:55 +0900

    2022-05-25 AM 01:07
    기능 변경사항
    
    3번째 스킬 최대한 버그 줄이고 투사체 발사(데미지 추후 조정) (playerManager - fire함수, bulletManager)
    슬라임 몬스터 이동방식 점프로 변경, 일정거리 접근시 근접공격(동작속도, 이동 거리, 공격 범위등 추후 조정) (enemyManager - slimeAI 함수)
    몬스터 근접공격, 몸빵 피격시 플레이어 데미지 받게 만듬(추후 수치조정)(playerManager - onDamaged함수)
    
    파일 변경사항
    Assets
    
    Images
    	Enemy - slime1_attack 2 추가
    	Effect - skill3_bullet 123 추가
    
    Others
    
    	Animations
    		Effect 폴더와 내용물 추가
    		Enemy - Slime - slime_attack 추가
    
    		Prefebs - Bullet123 추가
    
    Scripts
    	bulletManager 추가
    	enemyManager 수정
    	playerManager 수정
    
    버그 사항
    
    왠지 모르겠는데 만들다 보니 몬스터 공격시 데미지 뜨는거에 파란색 T가 같이 뜨는디 도저히 못고치것어요 헬프점

---
+++ Assets/Scripts/bulletManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletManager : MonoBehaviour
+{
+    private float rotation = 0;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (playerManager.flipx)
+        {
+            rotation = -1;
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            rotation = 1;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.right * rotation * 0.7f);
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("bullet1") && (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)) Destroy(gameObject);
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("bullet2") && (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)) Destroy(gameObject);
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("bullet3") && (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)) Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<enemyManager>().enemyDamaged(50);
+        }
+            
+    }
+}
