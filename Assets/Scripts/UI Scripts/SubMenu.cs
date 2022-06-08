a62f23c15eeb67f88a5988ad31f6d800630a8bbe
DevKMI <bkm0903v@naver.com>
2022-06-09 04:21:22 +0900

    ESC메뉴
    ESC메뉴

---
+++ Assets/Scripts/UI Scripts/SubMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Submenu;
+    [SerializeField]
+    private GameObject Subfade;
+    bool ActiveSubMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActiveSubMenu = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ActiveSubMenu = !ActiveSubMenu;
+            Submenu.SetActive(ActiveSubMenu);
+            Subfade.SetActive(ActiveSubMenu);
+        }
+    }
+}
