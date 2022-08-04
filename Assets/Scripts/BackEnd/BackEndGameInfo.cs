ee8e5094f91ef8c67062b053d2c922645d850b55
jaycoap <rooqoxo@gmail.com>
2022-08-04 18:19:13 +0900

    2022-08-04 PM18:18
    GameManager.cs, PlayerInfoPanel.cs, BackEndGameInfo.cs 현재 DB데이터 값 받아오기 테스트중

--- Assets/Scripts/BackEnd/BackEndGameInfo.cs
+++ Assets/Scripts/BackEnd/BackEndGameInfo.cs
@@ -3,29 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using BackEnd;
 using LitJson;
+using UnityEngine.UI;
+using System;
+
 
 public class BackEndGameInfo : MonoBehaviour
 {
+
+    public static BackEndGameInfo instance;
+
+    private void Awake() { instance = this; }
+
     string firstKey = string.Empty;
     GameManager gameManager;
-    public int q;
-    public int w;
-    public int e;
-    public int r;
-    public int t;
-    public int u;   
-    public int v;
-    public int x;
-    public int y;
-    public int z;
-    public int n;
-    public int l;
+    public int Level;
+    public int EXP;
+    public int MaxEXP;
+    public int HP;
+    public int MaxHP;
+    public int MP;   
+    public int MaxMP;
+    public int STR;
+    public int INT;
+    public int FIT;
+    public int APPoint;
+    public string getIndate;
+    public string getowner = "2022-07-25T05:47:22.446Z";
+    public string getname = "2022-07-25T05:47:22.110Z";
 
     
-    void update()
+
+    
+    void Update()
     {
         
     }
+    
+
+    // 개인 프라이빗 테이블 정보 가져오기 
+
+    [System.Obsolete]
+
+    
     public void OnClickInsertData()
     {
         int charLevel = GameManager.Instance.getLevel();
@@ -40,18 +59,7 @@ public class BackEndGameInfo : MonoBehaviour
         int charFIT = GameManager.Instance.getFIT();
         int charAPPoint = GameManager.Instance.getAPPoint();
 
-        q = charLevel;
-        w = charEXP;
-        e = charMaxEXP;
-        r = charHP;
-        t = charMP;
-        u = charSTR;
-        v = charINT;
-        x = charFIT;
-        y = charAPPoint;
-        z = charMP;
-        n = charMaxHP;
-        l = charMaxMp;
+        
 
         Param param = new Param();
 
@@ -90,6 +98,7 @@ public class BackEndGameInfo : MonoBehaviour
         if (BRO.IsSuccess())
         {
             Debug.Log("indate:" + BRO.GetInDate());
+            getIndate = BRO.GetInDate();
         }
         else
         {
@@ -114,13 +123,16 @@ public class BackEndGameInfo : MonoBehaviour
         }
     }
 
+    
     public void OnClickGetPrivateContents()
     {
-        BackendReturnObject BRO = Backend.GameInfo.GetPrivateContents("character");
-
+        BackendReturnObject BRO = Backend.GameData.GetV2("character", "inDate", getowner);
+       
         if (BRO.IsSuccess())
         {
             GetGameInfo(BRO.GetReturnValuetoJSON());
+            Debug.Log(BRO);
+            Debug.Log("Pass");
         }
         else
         {
@@ -130,27 +142,26 @@ public class BackEndGameInfo : MonoBehaviour
 
     void GetGameInfo(JsonData returnData)
     {
-        
+
         if (returnData != null)
         {
             Debug.Log("데이터가 존재합니다.");
-            
-            if (returnData.Keys.Contains("rows")) 
+
+            if (returnData.Keys.Contains("rows"))
             {
                 JsonData rows = returnData["rows"];
-
                 for (int i = 0; i < rows.Count; i++)
                 {
-                    
                     GetData(rows[i]);
-                    
                 }
-                
             }
             
-            else if (returnData.Keys.Contains("row"))
+
+            // row 로 전달받은 경우
+            else if (returnData.Keys.Contains("rows"))
             {
                 JsonData row = returnData["row"];
+                Debug.Log("Check");
                 GetData(row[0]);
             }
         }
@@ -158,28 +169,28 @@ public class BackEndGameInfo : MonoBehaviour
         {
             Debug.Log("데이터가 없습니다.");
         }
-        
+
 
     }
 
-    void GetData(JsonData data)
+    public void GetData(JsonData data)
     {
-        //var nickname = data["name"][0];
-        var character = data["character"];
+        Level = Int32.Parse(data["Level"][0].ToString());
+        Debug.Log(Level);
+        HP = Int32.Parse(data["HP"][0].ToString());
+        MaxHP = Int32.Parse(data["MaxHP"][0].ToString());
+        MP = Int32.Parse(data["MP"][0].ToString());
+        MaxMP = Int32.Parse(data["MaxMP"][0].ToString());
+        EXP = Int32.Parse(data["EXP"][0].ToString());
+        MaxEXP = Int32.Parse(data["MaxEXP"][0].ToString());
+        STR = Int32.Parse(data["STR"][0].ToString());
+        INT = Int32.Parse(data["INT"][0].ToString());
+        FIT = Int32.Parse(data["FIT"][0].ToString());
+        APPoint = Int32.Parse(data["APPoint"][0].ToString());
 
-       
-
-        if (data.Keys.Contains("Level"))
-        {
-            Debug.Log(data["Level"][0]);
-            Debug.Log(data["STR"][0]);
-        }
-        else
-        {
-            Debug.Log("Error");
-        }
-
+        SetDBLevel(Level);
     }
+    
     void CheckError(BackendReturnObject BRO)
     {
         switch (BRO.GetStatusCode())
@@ -223,6 +234,58 @@ public class BackEndGameInfo : MonoBehaviour
                 break;
 
         }
+    }
+
+    public int GetLevel()
+    {
+        
+        return Level;
+    }
+    public int GetHP()
+    {
+        return HP;
+    }
+    public int GetMaxHP()
+    {
+        return MaxHP;
+    }
+    public int GetMP()
+    {
+        return MP;
+    }
+    public int GetMaxMP()
+    {
+        return MaxMP;
+    }
+    public int GetEXP()
+    {
+        return EXP;
+    }
+    public int GetMaxEXP()
+    {
+        return MaxEXP;
+    }
+    public int GetSTR()
+    {
+        return STR;
+    }
+    public int GetINT()
+    {
+        return INT;
+    }
+    public int GetFIT()
+    {
+        return FIT;
+    }
+    public int GetAPPoint()
+    {
+        return APPoint;
+    }
+
+    public int SetDBLevel(int DBLevel)
+    {
+        Level = DBLevel;
+        return Level;
     }
     /*public void OnClickGameInfoUpdate()
     {
