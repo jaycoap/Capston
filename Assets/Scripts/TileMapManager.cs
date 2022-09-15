3f93eb74d88907cec54c46bbf2af8f898bf0969c
nogunga <78949961+nogunga@users.noreply.github.com>
2022-09-15 21:10:01 +0900

    발판 수정
    1차 수정

--- Assets/Scripts/TileMapManager.cs
+++ Assets/Scripts/TileMapManager.cs
@@ -48,7 +48,7 @@ public class TileMapManager : MonoBehaviour
                 Tilemap.SetTile(new Vector3Int(i, -2 - e, 0), tb.FloorTile);
             }
         }
-    }  
+    }
 
     private void MakePlatform()
     {
@@ -63,9 +63,9 @@ public class TileMapManager : MonoBehaviour
         //0은 할당 안된 좌표 1은 할당 된 좌표
         for (int i = 0; i < FloorWidth; i++)
         {
-            
+
             // -2 ~ 0 좌표는 플레이어 스폰 지역이기 때문에 제외
-            if(i >= -2 + halfWidth && i <= 1 + halfWidth)
+            if (i >= -2 + halfWidth && i <= 1 + halfWidth)
             {
                 platformX[i] = 1;
             }
@@ -79,10 +79,10 @@ public class TileMapManager : MonoBehaviour
         //시행회수가 너무 많을때 탈출하기 위한 변수
         int escape = 0;
         //크기가 5인 플렛폼 생성
-        for (int i = 0 ; i < platform.Platform5 ; i++)
+        for (int i = 0; i < platform.Platform5; i++)
         {
             //랜덤으로 X,Y 좌표를 고름 Y좌표는 2칸 간격으로
-            int R_X = Random.Range(8, (int)FloorWidth -8);
+            int R_X = Random.Range(8, (int)FloorWidth - 8);
             int R_Y = Random.Range(0, platform.Platform_Y_MAX) * 2;
             //체크를 한 구간에 할당된 좌표가 있는지 나타내는 변수
             bool X_Assignment = false;
@@ -106,26 +106,13 @@ public class TileMapManager : MonoBehaviour
             //발판 설치가 가능한지 체크
             if (!X_Assignment)
             {
-                for(int e = 0 ; e < R_Y / 2 ; e++)
+                for (int e = 0; e < R_Y / 2; e++)
                 {
                     //왼쪽에 할당된 발판 체크
                     if (platformX[R_X - e - 3] == 1)
                     {
                         //왼쪽에서 e + 1 번째 발판의 Y 좌표 차이가 2 + e*2 초과 이면 설치 불가능
-                        if(Mathf.Abs(platformY[R_X - e - 3] - R_Y) > 2 + e*2 )
-                        {
-                            Y_able = false;
-                            //발판 설치가 불가능해 플렛폼 설치를 못해서 i값 유지를 위해 -1
-                            i--;
-                            escape++;
-                        }
-                        
-                    }
-                    //오른쪽에 할당된 발판 체크
-                    if (platformX[R_X + e + 3] == 1)
-                    {
-                        //오른쪽에서 e + 1 번째 발판의 Y 좌표 차이가 2 + e*2 초과 이면 설치 불가능
-                        if (Mathf.Abs(platformY[R_X + e + 3] - R_Y) > 2 + e*2)
+                        if (Mathf.Abs(platformY[R_X - e - 3] - R_Y) > 2 + e * 2)
                         {
                             Y_able = false;
                             //발판 설치가 불가능해 플렛폼 설치를 못해서 i값 유지를 위해 -1
@@ -134,9 +121,22 @@ public class TileMapManager : MonoBehaviour
                         }
 
                     }
-                }    
+                    //오른쪽에 할당된 발판 체크
+                    if (platformX[R_X + e + 3] == 1)
+                    {
+                        //오른쪽에서 e + 1 번째 발판의 Y 좌표 차이가 2 + e*2 초과 이면 설치 불가능
+                        if (Mathf.Abs(platformY[R_X + e + 3] - R_Y) > 2 + e * 2)
+                        {
+                            Y_able = false;
+                            //발판 설치가 불가능해 플렛폼 설치를 못해서 i값 유지를 위해 -1
+                            i--;
+                            escape++;
+                        }
+
+                    }
+                }
             }
-            
+
             //5칸 안에 할당된 좌표가 없고 발판을 설치 해야 할 경우 설치 가능할때 플렛폼 설치 및 할당
             if (!X_Assignment && Y_able)
             {
@@ -176,27 +176,29 @@ public class TileMapManager : MonoBehaviour
                     else
                     {
                         //왼쪽에 거리가 1칸 이상 벌어진 발판이 있을시 발판설치  
-                        if(LplatformX >= 1)
+                        if (LplatformX >= 1)
                         {
-                            for (int o = 0 ; o < LplatformX ; o++)
+                            for (int o = 0; o < LplatformX; o++)
                             {
-                                //2칸 차이 날때
-                                if ((LplatformY - R_Y) / (LplatformX + 1) < 2)
+                                if (!(platformX[R_X - 3 - e] == 1))
                                 {
-                                    Tilemap.SetTile(new Vector3Int(R_X - Mathf.FloorToInt(halfWidth) - 3 - e, R_Y + (LplatformY - R_Y) / Mathf.Abs((LplatformY - R_Y)) * 2, 0), tb.FloorTile);
-                                    platformX[R_X - 3 - e] = 1;
-                                    platformY[R_X - 3 - e] = R_Y + (LplatformY - R_Y) / Mathf.Abs((LplatformY - R_Y)) * 2;
+                                    //2칸 차이 날때
+                                    if ((LplatformY - R_Y) / (LplatformX + 1) < 2)
+                                    {
+                                        Tilemap.SetTile(new Vector3Int(R_X - Mathf.FloorToInt(halfWidth) - 3 - e, R_Y + (LplatformY - R_Y) / Mathf.Abs((LplatformY - R_Y)) * 2, 0), tb.FloorTile);
+                                        platformX[R_X - 3 - e] = 1;
+                                        platformY[R_X - 3 - e] = R_Y + (LplatformY - R_Y) / Mathf.Abs((LplatformY - R_Y)) * 2;
+                                    }
+                                    //2칸 초과 차이 날때
+                                    else
+                                    {
+                                        Tilemap.SetTile(new Vector3Int(R_X - Mathf.FloorToInt(halfWidth) - 3 - e, R_Y + (LplatformY - R_Y) / (LplatformX + 1), 0), tb.FloorTile);
+                                        platformX[R_X - 3 - e] = 1;
+                                        platformY[R_X - 3 - e] = R_Y + (LplatformY - R_Y) / (LplatformX + 1);
+                                    }
                                 }
-                                //2칸 초과 차이 날때
-                                else
-                                {
-                                    Tilemap.SetTile(new Vector3Int(R_X - Mathf.FloorToInt(halfWidth) - 3 - e, R_Y + (LplatformY - R_Y) / (LplatformX + 1), 0), tb.FloorTile);
-                                    platformX[R_X - 3 - e] = 1;
-                                    platformY[R_X - 3 - e] = R_Y + (LplatformY - R_Y) / (LplatformX + 1);
-                                }
-                                
                             }
-                        }  
+                        }
                     }
                     if (!RplatformExist)
                     {
@@ -212,19 +214,22 @@ public class TileMapManager : MonoBehaviour
                         {
                             for (int o = 0; o < RplatformX; o++)
                             {
-                                //2칸 차이 날때
-                                if ((LplatformY - R_Y) / (LplatformX + 1) < 2)
+                                if (!(platformX[R_X + 3 + e] == 1))
                                 {
-                                    Tilemap.SetTile(new Vector3Int(R_X - Mathf.FloorToInt(halfWidth) - 3 - e, R_Y + (RplatformY - R_Y) / Mathf.Abs((RplatformY - R_Y)) * 2, 0), tb.FloorTile);
-                                    platformX[R_X - 3 - e] = 1;
-                                    platformY[R_X - 3 - e] = R_Y + (RplatformY - R_Y) / Mathf.Abs((RplatformY - R_Y)) * 2;
-                                }
-                                //2칸 초과 차이 날때
-                                else
-                                {
-                                    Tilemap.SetTile(new Vector3Int(R_X - Mathf.FloorToInt(halfWidth) - 3 - e, R_Y + (RplatformY - R_Y) / (RplatformX + 1), 0), tb.FloorTile);
-                                    platformX[R_X - 3 - e] = 1;
-                                    platformY[R_X - 3 - e] = R_Y + (RplatformY - R_Y) / (RplatformX + 1);
+                                    //2칸 차이 날때
+                                    if ((LplatformY - R_Y) / (LplatformX + 1) < 2)
+                                    {
+                                        Tilemap.SetTile(new Vector3Int(R_X - Mathf.FloorToInt(halfWidth) + 3 + e, R_Y + (RplatformY - R_Y) / Mathf.Abs((RplatformY - R_Y)) * 2, 0), tb.FloorTile);
+                                        platformX[R_X + 3 + e] = 1;
+                                        platformY[R_X + 3 + e] = R_Y + (RplatformY - R_Y) / Mathf.Abs((RplatformY - R_Y)) * 2;
+                                    }
+                                    //2칸 초과 차이 날때
+                                    else
+                                    {
+                                        Tilemap.SetTile(new Vector3Int(R_X - Mathf.FloorToInt(halfWidth) - 3 - e, R_Y + (RplatformY - R_Y) / (RplatformX + 1), 0), tb.FloorTile);
+                                        platformX[R_X + 3 + e] = 1;
+                                        platformY[R_X + 3 + e] = R_Y + (RplatformY - R_Y) / (RplatformX + 1);
+                                    }
                                 }
                             }
                         }
@@ -236,14 +241,20 @@ public class TileMapManager : MonoBehaviour
                 Tilemap.SetTile(new Vector3Int(R_X - Mathf.FloorToInt(halfWidth) - 2, R_Y, 0), tb.FloorTile);
                 Tilemap.SetTile(new Vector3Int(R_X - Mathf.FloorToInt(halfWidth) + 1, R_Y, 0), tb.FloorTile);
                 Tilemap.SetTile(new Vector3Int(R_X - Mathf.FloorToInt(halfWidth) + 2, R_Y, 0), tb.FloorTile);
-                for(int e = 0 ; e < 5 ; e++)
+                for (int e = 0; e < 5; e++)
                 {
                     platformX[R_X + e - 2] = 1;
                     platformY[R_X + e - 2] = R_Y;
                 }
-            }  
+                Debug.Log("순번");
+                Debug.Log(i);
+                Debug.Log("위치");
+                Debug.Log(R_X - Mathf.FloorToInt(halfWidth));
+
+                
+            }
             //시행회수가 너무 많을경우 탈출
-            if(escape > 100)
+            if (escape > 100)
             {
                 break;
             }
