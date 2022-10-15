1c04299a3c1ef42dc4edbd2c17ada1d898bb3b94
jaycoap <rooqoxo@gmail.com>
2022-10-15 15:50:07 +0900

    2022-10-15 PM 15:50
    StageManager.cs: 게임 스테이지 업데이트(수정중)

--- Assets/Scripts/BackEnd/StageManager.cs
+++ Assets/Scripts/BackEnd/StageManager.cs
@@ -22,7 +22,7 @@ public class StageManager : MonoBehaviour
     public string getIndate;
 
 
-    public void OnClickInsertData()
+    public void OnClickStageInsertData()
     {
         int clearstage = clearStage;
 
@@ -65,6 +65,55 @@ public class StageManager : MonoBehaviour
                     break;
             }
         }
+    }
+    public void OnClickGameInfoUpdate()
+    {
+        int clearstage = clearStage;
+
+        Param param = new Param();
+
+        param.Add("ClearStage", clearstage);
+
+        Dictionary<string, int> stage = new Dictionary<string, int>
+        {
+            {"ClearStage",clearstage }
+        };
+
+        param.Add("stage", stage);
+        BackendReturnObject BRO = Backend.GameData.Update("stage", getIndate, param);
+
+        if (BRO.IsSuccess())
+        {
+            Debug.Log("indate:" + BRO.GetInDate());
+            getIndate = BRO.GetInDate();
+
+        }
+        else
+        {
+            switch (BRO.GetStatusCode())
+            {
+                case "405":
+                    Debug.Log("param에 partition, gamer_id, inDate, updatedAt 네가지 필드가 있는 경우");
+                    break;
+
+                case "403":
+                    Debug.Log("퍼블릭테이블의 타인정보를 수정하고자 하였을 경우");
+                    break;
+
+                case "404":
+                    Debug.Log("존재하지 않는 tableName인 경우");
+                    break;
+
+                case "412":
+                    Debug.Log("비활성화 된 tableName인 경우");
+                    break;
+
+                case "413":
+                    Debug.Log("하나의 row( column들의 집합 )이 400KB를 넘는 경우");
+                    break;
+            }
+        }
+
     }
 
 
